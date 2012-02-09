bdfda0ee20d79a6a8cc949f6cd1616b044ed2c25
destinyd <destinyd_war@163.com>
2012-02-08 20:33:57 -0500

    backdb

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysqldump $1 -u root -p --default-character-set=utf8 > ~/$1.$(date "+%Y%m%d").sql
+mysqldump $1 -u root -p --default-character-set=utf8 --add-drop-table > ~/$1.$(date "+%Y%m%d").sql
