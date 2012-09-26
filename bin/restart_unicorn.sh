e7fd98dc8629bba80810479322a1a67cfebbf512
destinyd <destinyd_war@163.com>
2012-09-26 18:13:18 +0800

    fix

--- bin/restart_unicorn.sh
+++ bin/restart_unicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ps -ef | grep unicorn | grep -v grep | awk '{print $2}' | xargs kill -9
-ps -ef | grep unicorn | grep -v grep | awk '{print $2}'
+pkill -9 -f delayed_job
+pkill -9 -f unicorn
 RAILS_ENV=production script/delayed_job start
 unicorn_rails -c config/unicorn.rb -E production -D
