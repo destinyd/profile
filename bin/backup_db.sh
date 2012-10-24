2065eac7c9f75f8ddc4f89bd9483f362ef0061bd
destinyd <destinyd_war@163.com>
2012-10-24 10:49:20 -0400

    bin

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mysqldump $1 -u root -p --default-character-set=utf8 --add-drop-table > ~/$1.$(date "+%Y%m%d").sql
-tar cvfz ~/$1.$(date "+%Y%m%d").tar.gz ~/$1.$(date "+%Y%m%d").sql
-rm ~/$1.$(date "+%Y%m%d").sql
+mysqldump $1 -u root -p --default-character-set=utf8 --add-drop-table > $1.$(date "+%Y%m%d").sql
+tar czf $1.$(date "+%Y%m%d").tar.gz $1.$(date "+%Y%m%d").sql
+#rm ~/$1.$(date "+%Y%m%d").sql
--- bin/restart_unicorn.sh
+++ bin/restart_unicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill -9 -f delayed_job
-pkill -9 -f unicorn
+pkill -9 -f unicorn_rails
 RAILS_ENV=production script/delayed_job start
 unicorn_rails -c config/unicorn.rb -E production -D
